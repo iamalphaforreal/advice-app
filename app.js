028523e20e0a343e67b1ea5102789cbeffea3888
Alpha  Habib <habibalpha19@gmail.com>
2022-03-11 20:37:21 +0200

    removed the reload button and replaced it with a function

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-//const quote = document.getElementsByClassName("quote")
+const quote = document.getElementsByClassName("quote")
 
-fetch("https://api.adviceslip.com/advice")
+ 
+  const dice = document.querySelector("#dice");
+  dice.addEventListener("click", () => {
+    //fetch the data
+
+    fetch("https://api.adviceslip.com/advice")
   .then((data) => data.json())
   .then((completedata) => {
     console.log(completedata);
     document.getElementById("quote").innerText = completedata.slip.advice;
 
-    document.querySelector("#heading").innerText = (`Advice #${completedata.slip.id}`);
-    
-const dice = document.querySelector("#dice")
-dice.addEventListener("click",()=>{
-        window.location.reload();
-})
+    document.querySelector(
+      "#heading"
+    ).innerText = `Advice #${completedata.slip.id}`;
 
+  });
   });
